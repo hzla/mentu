f0d305c82ec4471556ea8a2664512632668631a1
hzla00 <andylee.hzl@gmail.com>
2015-01-05 15:54:12 -0800

    remove devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  devise_for :users, ActiveAdmin::Devise.config
+  # ActiveAdmin.routes(self)
   root to: 'pages#home'
   resources :users
 
