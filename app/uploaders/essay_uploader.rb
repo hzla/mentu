cd06b66ca62c60b50aad1c62a2eebe4bfba91866
hzla00 <andylee.hzl@gmail.com>
2015-01-05 17:57:11 -0800

    comment out code inside essay uploader class

--- app/uploaders/essay_uploader.rb
+++ app/uploaders/essay_uploader.rb
@@ -8,15 +8,13 @@ class EssayUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
-  include CarrierWave::MimeTypes
-	process :set_content_type
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/essays/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/essays/#{model.id}"
+  # end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
